57c5382cd2a8cab26b070478d6f4be2cf97e549c
Victorcvaz <victorcesarvaz2022@gmail.com>
2023-08-04 23:36:00 -0300

    fixed: ft_split.

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vcesar-v <vcesar-v@student.42.fr>          +#+  +:+       +#+        */
+/*   By: victorcvaz <victorcvaz@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 17:47:20 by vcesar-v          #+#    #+#             */
-/*   Updated: 2023/08/03 21:33:14 by vcesar-v         ###   ########.fr       */
+/*   Updated: 2023/08/04 23:28:30 by victorcvaz       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,26 @@
 
 static	int		fill_result(char **result, char const *s, char c);
 static	void		free_result(char **result, int result_index);
-static	char		*allocate_substring(char const *s, int sep_nbr, int last_i);
+static	char		*allocate_substring(char const *s, int matriz_i, int last_i, char c);
 
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
-	char	*s2;
-	size_t	sep_nbr;
+	size_t	matriz_i;
 	int		i;
 
-	i = -1;
-	sep_nbr = 0;
-	while (s[++i])
+	i = 0;
+	matriz_i = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i++])
 	{
-		if (s[i] == c && s[i + 1])
-			sep_nbr++;
+		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+			matriz_i++;
 	}
-	if (!s || !sep_nbr || sep_nbr == ft_strlen(s))
+	if (!s || !matriz_i)
 		return (NULL);
-	result = (char **)malloc(sizeof(char *) * (sep_nbr + 2));
+	result = (char **)malloc(sizeof(char *) * matriz_i + 1);
 	if (!result)
 		return (NULL);
 	if (!fill_result(result, s, c))
@@ -49,13 +50,14 @@ static	int	fill_result(char **result, char const *s, char c)
 	res_i = -1;
 	last_i = -1;
 	curr_i = 0;
-	while (s[++last_i])
+	while (s[++last_i] == c)
+		curr_i++;
+	while (s[last_i++])
 	{
-		if (s[last_i + 1] == '\0' || s[last_i] == c)
+		if ((s[last_i] == c && s[last_i + 1] != c) 
+			|| (s[last_i] != c && s[last_i + 1] == '\0'))
 		{
-			while (s[last_i] == c && s[last_i] != '\0')
-				last_i++;
-			result[++res_i] = allocate_substring(s, curr_i, last_i);
+			result[++res_i] = allocate_substring(s, curr_i , last_i, c);
 			if (!result[res_i])
 			{
 				free_result(result, res_i);
@@ -67,14 +69,16 @@ static	int	fill_result(char **result, char const *s, char c)
 	return (1);
 }
 
-static	char	*allocate_substring(char const *s, int curr_i, int last_i)
+static	char	*allocate_substring(char const *s, int curr_i, int last_i, char c)
 {
 	char	*substring;
 	size_t	i;
 	size_t	substr_len;
 
 	i = 0;
-	substr_len = last_i - curr_i;
+	while (s[last_i] == c)
+		last_i--;
+	substr_len = last_i - curr_i + 1;
 	substring = (char *)malloc(sizeof(char) * (substr_len + 1));
 	if (!substring)
 		return (NULL);
@@ -89,7 +93,7 @@ static	void	free_result(char **result, int result_index)
 	int	i;
 
 	i = -1;
-	while (++i < result_index)
+	while (++i <= result_index)
 		free(result[i]);
 	free(result);
 }
--- main.c
+++ main.c
@@ -3,5 +3,16 @@
 
 int main()
 {
-	
+	char **result;
+	char **result2;
+	char *str = "Hello,,World";
+	result = ft_split(str, ',');
+	printf("%s\n", result[0]);
+	printf("%s\n", result[1]);
+	printf("\n");
+	char *str2 = ",,,HelloWorld,";
+	result2 = ft_split(str2, ',');
+	// printf("%s\n", result2[0]);
+	// printf("%s\n", result2[1]);
+	return (0);
 }
