a361bd7f5aa5686c0ba5e37e6f55d362b474d18c
Victorcvaz <victorcesarvaz2022@gmail.com>
2023-08-09 01:03:36 -0300

    fixed: ft_strrchr. (not casting int as char)

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,24 +6,26 @@
 /*   By: victorcvaz <victorcvaz@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 17:54:00 by vcesar-v          #+#    #+#             */
-/*   Updated: 2023/08/08 23:07:18 by victorcvaz       ###   ########.fr       */
+/*   Updated: 2023/08/09 00:59:02 by victorcvaz       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	size_t	str_len;
+	char	*str;
 
-	i = 0;
-	while (s[i])
-		i++;
-	if (c == '\0')
-		return ((char *)s + i);
-	while (i > 0)
+	str_len = ft_strlen(s);
+	str = (char *)s;
+	if ((char) c == '\0')
+		return (str + str_len);
+	while (str_len > 0)
 	{
-		i--;
-		if ((char)s[i] == c)
-			return ((char *)s + i);
+		str_len--;
+		if (str[str_len] == (char) c)
+			return (str + str_len);
 	}
-	return (0);
+	return (NULL);
 }
