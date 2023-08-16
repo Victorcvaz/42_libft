834999289d3483d180cc2f1ac703921a42077d68
Victor Cesar Vaz <vcesar-v@c1r9p4.42sp.org.br>
2023-08-16 07:35:47 -0300

    feat: putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victorcvaz <victorcvaz@student.42.fr>      +#+  +:+       +#+        */
+/*   By: vcesar-v <vcesar-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 17:47:04 by vcesar-v          #+#    #+#             */
-/*   Updated: 2023/08/14 02:14:21 by victorcvaz       ###   ########.fr       */
+/*   Updated: 2023/08/16 07:34:36 by vcesar-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ void	ft_putnbr_fd(int n, int fd)
 	char	*result;
 
 	result = ft_itoa(n);
+	if (!result)
+	{
+		ft_putstr_fd("Error: Failed to allocate memory\n", fd);
+		return ;
+	}
 	ft_putstr_fd(result, fd);
 	free(result);
 }
