09dcd028c0d93f11fed6c2d70600e086602dcde7
Victorcvaz <victorcesarvaz2022@gmail.com>
2023-09-12 04:54:34 -0300

    fixed: handle null

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vcesar-v <vcesar-v@student.42.fr>          +#+  +:+       +#+        */
+/*   By: victorcvaz <victorcvaz@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 17:53:35 by vcesar-v          #+#    #+#             */
-/*   Updated: 2023/08/03 19:55:23 by vcesar-v         ###   ########.fr       */
+/*   Updated: 2023/09/12 04:52:46 by victorcvaz       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	count;
 
+	if (!s)
+		return (0);
 	count = 0;
 	while (s[count])
 		count++;
