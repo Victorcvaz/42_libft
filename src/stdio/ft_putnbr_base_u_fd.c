fa7c76189d825e8ac59ba0c7894ca7253c95085d
Victorcvaz <victorcesarvaz2022@gmail.com>
2023-10-01 20:20:39 -0300

    feat: add fn ft_putnbr_base_u_fd. up ft_putnbr_base_u_fd

--- src/stdio/ft_putnbr_base_u_fd.c
+++ src/stdio/ft_putnbr_base_u_fd.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_base_fd.c                                :+:      :+:    :+:   */
+/*   ft_putnbr_base_u_fd.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victorcvaz <victorcvaz@student.42.fr>      +#+  +:+       +#+        */
+/*   By: vcesar-v <vcesar-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 10:07:22 by victorcvaz        #+#    #+#             */
-/*   Updated: 2023/09/29 09:36:30 by victorcvaz       ###   ########.fr       */
+/*   Updated: 2023/09/30 16:55:34 by vcesar-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../libft.h"
+#include "../includes/ft_printf.h"
 
-ssize_t	ft_putnbr_base_fd(long int nbr, char *base, int fd)
+ssize_t	ft_putnbr_base_u_fd(unsigned long int nbr, char *base, int fd)
 {
 	ssize_t	total_bytes;
 	size_t	base_len;
@@ -21,13 +21,8 @@ ssize_t	ft_putnbr_base_fd(long int nbr, char *base, int fd)
 		return (0);
 	base_len = ft_strlen(base);
 	total_bytes = 0;
-	if (nbr < 0)
-	{
-		total_bytes += ft_putchar_fd('-', fd);
-		nbr = -nbr;
-	}
-	if (nbr >= (long int)base_len)
-		total_bytes += ft_putnbr_base_fd(nbr / base_len, base, fd);
+	if (nbr >= base_len)
+		total_bytes += ft_putnbr_base_u_fd(nbr / base_len, base, fd);
 	total_bytes += ft_putchar_fd(base[nbr % base_len], fd);
 	return (total_bytes);
 }
