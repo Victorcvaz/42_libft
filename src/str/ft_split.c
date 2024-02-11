7177aed1c29459313ec6ea297bcf93b469f1fb3c
Victorcvaz <victorcesarvaz2022@gmail.com>
2024-02-11 04:37:18 -0300

    add: new ft_split

--- src/str/ft_split.c
+++ src/str/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: victorcvaz <victorcvaz@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 17:47:20 by vcesar-v          #+#    #+#             */
-/*   Updated: 2023/09/14 20:58:24 by victorcvaz       ###   ########.fr       */
+/*   Updated: 2024/02/11 04:36:41 by victorcvaz       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,76 +15,71 @@
 static	int	ft_fill_result(char **result, char const *s, char c, int matriz_i);
 static	int	ft_count_words(const char *str, char c);
 
+static size_t	count_words(char const *s, char c)
+{
+	size_t	i;
+	size_t	words;
+
+	words = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i] && s[i] != c)
+			words++;
+		while (s[i] && s[i] != c)
+			i++;
+	}
+	return (words);
+}
+
+static size_t	ft_word_len(char const *str, char c, size_t start)
+{
+	size_t	w_len;
+
+	w_len = 0;
+	while (str[start] != c && str[start] != '\0')
+	{
+		w_len++;
+		start++;
+	}
+	return (w_len);
+}
+
+static void	ft_free(char **s, size_t current_word)
+{
+	while (current_word > 0)
+		free(s[current_word--]);
+	free(s);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	char	**result;
-	size_t	matriz_i;
-	size_t	i;
+	char	**split;
+	size_t	start;
+	size_t	words;
+	size_t	current_word;
+	size_t	word_len;
 
-	if (!s)
+	words = count_words(s, c);
+	split = (char **)ft_calloc((words + 1), sizeof(char *));
+	if (split == NULL)
 		return (NULL);
-	matriz_i = ft_count_words(s, c);
-	result = (char **)malloc(sizeof(char *) * (matriz_i + 1));
-	if (!result)
-		return (NULL);
-	if (!ft_fill_result(result, s, c, matriz_i))
+	start = 0;
+	current_word = -1;
+	while (++current_word < words)
 	{
-		i = 0;
-		while (result[i] != NULL)
-		{
-			free(result[i]);
-			i++;
-		}
-		free(result);
-		return (NULL);
+		while (s[start] == c)
+			start++;
+		word_len = ft_word_len(s, c, start);
+		split[current_word] = ft_substr(s, start, word_len);
+		if (split[current_word] == NULL)
+			ft_free (split, current_word);
+		if (split == NULL)
+			return (NULL);
+		start += word_len;
 	}
-	return (result);
+	return (split);
 }
 
-static	int	ft_fill_result(char **result, char const *s, char c, int matriz_i)
-{
-	int	res_i;
-	int	curr_i;
-	int	word_len;
-
-	res_i = 0;
-	curr_i = 0;
-	while (s[curr_i] && res_i < matriz_i)
-	{
-		word_len = 0;
-		while (s[curr_i] == c && s[curr_i])
-			curr_i++;
-		while (s[curr_i + word_len] != c && s[curr_i + word_len])
-			word_len++;
-		result[res_i] = ft_substr(s, curr_i, word_len);
-		if (result[res_i] == NULL)
-			return (0);
-		res_i++;
-		curr_i += word_len + 1;
-	}
-	result[res_i] = NULL;
-	return (1);
-}
-
-static	int	ft_count_words(const char *s, char c)
-{
-	int		i;
-	int		trigger;
-	char	*str;
-
-	i = 0;
-	trigger = 0;
-	str = (char *) s;
-	while (*str)
-	{
-		if (*str != c && trigger == 0)
-		{
-			trigger = 1;
-			i++;
-		}
-		else if (*str == c)
-			trigger = 0;
-		str++;
-	}
-	return (i);
-}
